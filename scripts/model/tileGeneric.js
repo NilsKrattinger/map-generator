d86533d05c26e709a876960eab9336e654da8995
Angèle MATEOS <angele_m@orange.fr>
2021-01-05 11:58:42 +0100

    retour à la ligne

--- scripts/model/tileGeneric.js
+++ scripts/model/tileGeneric.js
@@ -6,7 +6,7 @@ const TileGeneric = {
     humidity: undefined,
 
     //no setters or getters necessary for the moment
-    
+
     //possibility to implement later coordinates for 
     //the different positions of the tile on the map
 
